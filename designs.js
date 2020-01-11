2a8bb178237211880fa9069493bc2a365b6d49af
Josh Dial <jddial@hotmail.com>
2020-01-11 15:55:43 +0900

    Update designs.js

--- designs.js
+++ designs.js
@@ -5,16 +5,26 @@ const color = document.getElementById('colorPicker');
 const gridHeight = document.getElementById('inputHeight');
 const gridWidth = document.getElementById('inputWidth');
 const gridSize = document.getElementById('sizePicker');
+const designCanvas = document.getElementById('pixelCanvas');
 
 // When size is submitted by the user, call makeGrid()
-
-function makeGrid() {
-
-// Your code goes here!
-
-for(var row = 0; row < numbers.length; row++){
-    for(var column = 0; column < numbers[row].length; column++){
-       
-}
-
-}
+document.addEventListener('submit', function() {
+    event.preventDefault();
+    const width = gridWidth.value;
+    const height = gridHeight.value;
+    pixelCanvas.innerHTML = "";
+    makeGrid(height, width);
+});
+function makeGrid(height, width) {
+    for (w = 0; w <= width; ++w) {
+        let row = document.createElement('tr');
+        for (h = 0; h <= height; ++h) {
+            let cell = document.createElement('td');
+            row.appendChild('cell');
+           cell.addEventListener('ondrag', function(events) {
+              cell.style.backgroundColor = colorPicker.value;
+            })
+        };
+        pixelCanvas.appendChild('row');
+    };
+};

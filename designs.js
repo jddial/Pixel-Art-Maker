53ce0039c3ee04b4ded200621cac92da9a8cd378
Josh Dial <jddial@hotmail.com>
2020-01-11 17:32:36 +0900

    Update designs.js

--- designs.js
+++ designs.js
@@ -6,25 +6,34 @@ var gridHeight = document.getElementById('inputHeight');
 var gridWidth = document.getElementById('inputWidth');
 var gridSize = document.getElementById('sizePicker');
 var designCanvas = document.getElementById('pixelCanvas');
+var selectedGrid = document.getElementById('td');
 
 // When size is submitted by the user, call makeGrid()
 document.addEventListener('submit', function() {
     event.preventDefault();
     var width = gridWidth.value;
     var height = gridHeight.value;
-    var color = pickAColor.value;    
     makeGrid(height, width);
 });
 // Make table
 function makeGrid(height, width) {
-    pixelCanvas.innerHTML = "";
+    designCanvas.innerHTML = "";
     for (var w = 1; w <= width; w++) {
         let row = document.createElement('tr');
         for(var h = 1; h <= height; h++){
             let col = document.createElement('td');
             row.appendChild(col);
+            col.addEventListener('click', function(event) {
+                col.style.backgroundColor = pickAColor.value; })
+            col.addEventListener('dblclick', function(event) {
+                col.style.backgroundColor = 'white';
+            })
         }
-        pixelCanvas.appendChild(row);
+        designCanvas.appendChild(row);
     }
 }
 // Set color for selected cell
+selectedGrid.addEventListener('click', function() {
+    event.preventDefault();
+    console.log(selectedGrid.value);
+})
